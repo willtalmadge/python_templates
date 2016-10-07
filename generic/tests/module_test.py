efcacb103c1e87e855addd72d2a742fa8bcc9f25
William Talmadge <willtalmadge@gmail.com>
2016-10-06 18:28:03 -0600

    Added some info about developer installs

--- generic/tests/module_test.py
+++ generic/tests/module_test.py
@@ -1,5 +1,8 @@
 """
 Run the test suite with `python setup.py test`
+
+Don't add an __init__.py to this directory to make it a module. Find another way to do what you want. If it's a module
+it could get installed and put `tests` into the global namespace.
 """
 
 import unittest
