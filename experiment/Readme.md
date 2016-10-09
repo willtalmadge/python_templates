cae8b070f7d5b6a5dbb163d8f46ea65e0426fc0b
William Talmadge <willtalmadge@gmail.com>
2016-10-09 15:21:49 -0600

    Added some features, will flesh this out with an actual experiment

--- experiment/Readme.md
+++ experiment/Readme.md
@@ -1,10 +1,1 @@
-This project is a template for a python command line application and a library. 
-Run tests with `python setup.py test`
-
-Install with `python setup.py install` or `python setup.py develop`
-
-When installing with develop, be careful about how the environment might get modified with junk,
-read more here
-http://blog.olgabotvinnik.com/blog/2014/03/03/2014-03-03-pythonpath-is-a-liar-site-py-and-easy-install-pth-tell/
-
-Run the sample command with `my_cmd 2`
+This is a project template for an experiment.
--- experiment/my_experiment/module.py
+++ experiment/my_experiment/module.py
@@ -1,25 +1,51 @@
 import subprocess
 import os
 import logging
+import warnings
 
-def get_svn_revision(file):
-    lines = subprocess.check_output(['svn', 'status', file])
-    filename = os.path.split(file)[1]
-    for line in lines.splitlines():
-        if b'is not a working copy' in line:
-            return None
-        if filename in line:
-            raise RuntimeError('The svn revision of ({0}) cannot be determined since it is not committed.')
+
+def guard_svn_id(file, stop_if_not_versioned=False):
+    """
+    This is a help function that protects against accidentally running experiments with source code that isn't
+    checked into source control. This is a problem because it's not reproducible. This function will stop
+    the program if the file that calls this is not under version control at all. It will optionally stop the
+    program if the file has been modified. It's recommended to set stop_if_not_version based on an environment
+    variable. (1 for true, 0 for false).
+
+    The return value of this function is intended to be used as a log name.
+
+    :param file:
+    :param stop_if_not_versioned:
+    :return:
+    """
+    if isinstance(stop_if_not_versioned, str):
+        if stop_if_not_versioned == '1':
+            stop = True
+        else:
+            stop = False
+    else:
+        stop = stop_if_not_versioned
 
     version = subprocess.check_output(['svnversion', file])
     if b'Unversioned' in version:
-        return None
-    else:
-        return int(version)
+        raise RuntimeError('The file {0} is unversioned, check it into version control before running'.format(file))
+    if b'M' in version:
+        msg = 'The file {0} has been modified, but not checked in, check it in before running'.format(file)
+        if stop:
+            raise RuntimeError(msg)
+        else:
+            warnings.warn(msg)
+
+    id = file
+    lines = subprocess.check_output(['svn', 'status', file])
+    for line in lines.splitlines():
+        if b'URL:' in line:
+            id = line.replace(b'URL: ', b'').strip()
+
+    return '{0}@{1}'.format(id.decode(), version.decode())
 
 
-svn = get_svn_revision(__file__)
-logger = logging.getLogger(__name__ + '@' + str(svn))
+logger = logging.getLogger(guard_svn_id(__file__, stop_if_not_versioned=not int(os.environ['DEBUG'])))
 logger.setLevel(logging.INFO)
 
 formatter = logging.Formatter('%(asctime)s ~ %(name)s ~ %(levelname)s ~ %(message)s')
